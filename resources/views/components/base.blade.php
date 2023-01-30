fa2683954661177bab0bde6180d0b824d4cc0148
robs007 <rszabo@crowleyauto.net>
2023-01-30 11:49:25 -0500

    last update

--- resources/views/components/base.blade.php
+++ resources/views/components/base.blade.php
@@ -4,7 +4,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
     <!-- The CSS stylesheet which will be inlined. -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('build/assets/app.ea9d52bc.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('build/assets/app.850f945c.css') }}">
 
 {{--    @vite(['resources/css/mail.css'])--}}
 
